9df43a4ea73013b10f4bf54f5674528fcb63ede3
bakchan <chan.park@howoocast.com>
2024-03-15 10:33:35 +0900

    다중 이미지구역 만들기

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -6,6 +6,7 @@ import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PostArticle from "@/app/(afterLogin)/_component/PostArticle"
 import {faker} from '@faker-js/faker'
+import PostImages from '@/app/(afterLogin)/_component/PostImages'
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
@@ -29,9 +30,9 @@ export default function Post({ noImage }: Props) {
   if (Math.random() > 0.5 && !noImage) {
     target.Images.push(
       {imageId: 1, link: faker.image.urlLoremFlickr()},
-      // {imageId: 2, link: faker.image.urlLoremFlickr()},
-      // {imageId: 3, link: faker.image.urlLoremFlickr()},
-      // {imageId: 4, link: faker.image.urlLoremFlickr()},
+      {imageId: 2, link: faker.image.urlLoremFlickr()},
+      {imageId: 3, link: faker.image.urlLoremFlickr()},
+      {imageId: 4, link: faker.image.urlLoremFlickr()},
     )
   }
 
@@ -58,14 +59,7 @@ export default function Post({ noImage }: Props) {
           </div>
           <div>{target.content}</div>
           <div className={style.postImageSection}>
-            {target.Images && target.Images.length > 0 && (
-              <Link 
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={style.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt=""/>
-              </Link>
-            )}
+            <PostImages post={target} />
           </div>
           <ActionButtons />
         </div>
