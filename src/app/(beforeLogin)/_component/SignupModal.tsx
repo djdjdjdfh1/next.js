ad3b2a699750be8738ed908b0a6536e7580921b4
bakchan <chan.park@howoocast.com>
2024-03-15 11:23:38 +0900

    서버 컴퍼넌트 9분

--- src/app/(beforeLogin)/_component/SignupModal.tsx
+++ src/app/(beforeLogin)/_component/SignupModal.tsx
@@ -1,13 +1,42 @@
 import style from './signup.module.css';
 import {ChangeEventHandler, FormEventHandler, useState} from "react";
 import BackButton from '@/app/(beforeLogin)/_component/BackButton'
+import { redirect } from 'next/navigation';
 
 export default function SignupModal() {
   const submit = async(formData: FormData) => {
     "use server";
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`, {
-      
-    })
+    if (!formData.get('id')) {
+      return { message: 'no_id' };
+    }
+    if (!formData.get('name')) {
+      return { message: 'no_name' };
+    }
+    if (!formData.get('password')) {
+      return { message: 'no_password' };
+    }
+    if (!formData.get('image')) {
+      return { message: 'no_image' };
+    }
+    let shouldRedirect = false;
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`, {
+        method: 'post',
+        body: formData,
+        credentials: 'include',
+      })
+      console.log(response.status)
+      if (response.status === 403) {
+        return { message: 'user_exists' };
+      }
+      console.log(await response.json())
+      shouldRedirect = true;
+    } catch (err) {
+      console.error(err)
+    }
+    if (shouldRedirect) {
+      redirect('/home'); // try/catch안에서 쓰면 안됨
+    }
   }
 
   return (
