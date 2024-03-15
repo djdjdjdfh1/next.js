9ab3ddba84b73d3884a6b2c2ca262a1a4b4cb2d6
bakchan <chan.park@howoocast.com>
2024-03-15 11:01:15 +0900

    msw 세팅과 버전 업그레이드 완료

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,21 @@
+import {http, HttpResponse, StrictResponse} from 'msw'
+import {faker} from "@faker-js/faker";
+
+export const handlers = [
+    http.post('/api/login', () => {
+      console.log('로그인');
+      return HttpResponse.json({
+        headers: {
+          'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+        }
+      })
+    }),
+    http.post('/api/logout', () => {
+      console.log('로그아웃');
+      return new HttpResponse(null, {
+        headers: {
+          'Set-Cookie': 'connect.sid=;HttpOnly;Path=/;Max-Age=0'
+        }
+      })
+    }),
+]
