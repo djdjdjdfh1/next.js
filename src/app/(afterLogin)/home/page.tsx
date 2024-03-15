2f3a8bc3aeec8489c2a01e334d77cb84dab57447
bakchan <chan.park@howoocast.com>
2024-03-15 15:04:00 +0900

    클라이언트 리액트 쿼리 핸들러 수정 뺴고 완

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -2,25 +2,9 @@ import style from './home.module.css';
 import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
-import Post from "@/app/(afterLogin)/_component/Post";
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
-
-async function getPostRecommends() {
-  const res = await fetch(`http://localhost:9090/api/postRecommends`, {
-    next: {
-      tags: ['posts', 'recommends'],
-    },
-    cache: 'no-store',
-  });
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json();
-}
-
-
+import { getPostRecommends } from '@/app/(afterLogin)/home/_lib/getPostRecommends';
+import PostRecommends from '@/app/(afterLogin)/home/_component/PostRecommends';
 
 export default async function Home() {
   const queryClient = new QueryClient();
@@ -33,18 +17,7 @@ export default async function Home() {
         <TabProvider>
           <Tab/>
           <PostForm />
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
-          <Post/>
+          <PostRecommends />
         </TabProvider>
       </HydrationBoundary>
     </main>
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,20 @@
 import {http, HttpResponse, StrictResponse} from 'msw'
 import {faker} from "@faker-js/faker";
 
+function generateDate() {
+  const lastWeek = new Date(Date.now());
+  lastWeek.setDate(lastWeek.getDate() - 7);
+  return faker.date.between({
+    from: lastWeek,
+    to: Date.now(),
+  });
+}
+const User = [
+  {id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg'},
+  {id: 'zerohch0', nickname: '제로초', image: '/5Udwvqim.jpg'},
+  {id: 'leoturtle', nickname: '레오', image: faker.image.avatar()},
+]
+
 export const handlers = [
     http.post('/api/login', () => {
       console.log('로그인');
@@ -25,10 +39,66 @@ export const handlers = [
     }),
     http.post('/api/users', async ({ request }) => {
       console.log('회원가입');
+        // return HttpResponse.text(JSON.stringify('user_exists'), {
+        //   status: 403,
+        // })
       return HttpResponse.text(JSON.stringify('ok'), {
         headers: {
           'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0'
         }
       })
     }),
-]
+    http.post('/api/postRecommends', async({ request }) => {
+      return HttpResponse.json(
+        [
+          {
+            postId: cursor + 1,
+            User: User[0],
+            content: `${cursor + 1} Z.com is so marvelous. I'm gonna buy that.`,
+            Images: [{imageId: 1, link: faker.image.urlLoremFlickr()}],
+            createdAt: generateDate(),
+          },
+          {
+            postId: cursor + 2,
+            User: User[0],
+            content: `${cursor + 2} Z.com is so marvelous. I'm gonna buy that.`,
+            Images: [
+              {imageId: 1, link: faker.image.urlLoremFlickr()},
+              {imageId: 2, link: faker.image.urlLoremFlickr()},
+            ],
+            createdAt: generateDate(),
+          },
+          {
+            postId: cursor + 3,
+            User: User[0],
+            content: `${cursor + 3} Z.com is so marvelous. I'm gonna buy that.`,
+            Images: [],
+            createdAt: generateDate(),
+          },
+          {
+            postId: cursor + 4,
+            User: User[0],
+            content: `${cursor + 4} Z.com is so marvelous. I'm gonna buy that.`,
+            Images: [
+              {imageId: 1, link: faker.image.urlLoremFlickr()},
+              {imageId: 2, link: faker.image.urlLoremFlickr()},
+              {imageId: 3, link: faker.image.urlLoremFlickr()},
+              {imageId: 4, link: faker.image.urlLoremFlickr()},
+            ],
+            createdAt: generateDate(),
+          },
+          {
+            postId: cursor + 5,
+            User: User[0],
+            content: `${cursor + 5} Z.com is so marvelous. I'm gonna buy that.`,
+            Images: [
+              {imageId: 1, link: faker.image.urlLoremFlickr()},
+              {imageId: 2, link: faker.image.urlLoremFlickr()},
+              {imageId: 3, link: faker.image.urlLoremFlickr()},
+            ],
+            createdAt: generateDate(),
+          },
+        ]
+      )
+    }),
+];
