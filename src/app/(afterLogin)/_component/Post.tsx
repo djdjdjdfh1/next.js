86775f2f8371d07edac50ac40d20141f4552a66e
bakchan <chan.park@howoocast.com>
2024-03-15 09:30:49 +0900

    페이커 코딩도중 오류

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -5,6 +5,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PostArticle from "@/app/(afterLogin)/_component/PostArticle"
+import {faker} from '@faker-js/faker'
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
@@ -19,8 +20,18 @@ export default function Post() {
     },
     content: '언제쯤 완성이 될까요?',
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   }
+
+  if (Math.random() > 0.5) {
+    target.Images.push(
+      {imageId: 1, link: faker.image.urlLoremFlickr()},
+      // {imageId: 2, link: faker.image.urlLoremFlickr()},
+      // {imageId: 3, link: faker.image.urlLoremFlickr()},
+      // {imageId: 4, link: faker.image.urlLoremFlickr()},
+    )
+  }
+
   return (
     <PostArticle post={target}>
       <div className={style.postWrapper}>
@@ -44,7 +55,14 @@ export default function Post() {
           </div>
           <div>{target.content}</div>
           <div className={style.postImageSection}>
-
+            {target.Images && target.Images.length > 0 && (
+              <Link 
+                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
+                className={style.postImageSection}
+              >
+                <img src={target.Images[0]?.link} alt=""/>
+              </Link>
+            )}
           </div>
           <ActionButtons />
         </div>
