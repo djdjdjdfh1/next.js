0cf048fa3bacb63d575f8ce4f6269e797ce72fa0
bakchan <chan.park@howoocast.com>
2024-03-15 14:42:09 +0900

    리액트쿼리 SSR 설정하기 완

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -4,10 +4,26 @@ import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
 import Post from "@/app/(afterLogin)/_component/Post";
 import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+import { revalidatePath } from 'next/cache';
 
 async function getPostRecommends() {
+  const res = await fetch(`http://localhost:9090/api/postRecommends`, {
+    next: {
+      tags: ['posts', 'recommends'],
+    },
+    cache: 'no-store',
+  });
 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  revalidatePath('/home');
+
+  return res.json();
 }
+
+
 
 export default async function Home() {
   const queryClient = new QueryClient();
