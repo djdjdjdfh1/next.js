957af47c721ae46994e051a4ff3ed57cc67f76a8
bakchan <chan.park@howoocast.com>
2024-03-19 13:55:10 +0900

    풀 라우터 캐시와 라우터 캐시 - 로그아웃후 다른아이디 로그인시 계정 중복 수정

--- src/app/(afterLogin)/_component/LogoutButton.tsx
+++ src/app/(afterLogin)/_component/LogoutButton.tsx
@@ -27,6 +27,7 @@ export default function LogoutButton({ me }: Props) {
           method: 'post',
           credentials: 'include',
         })
+        router.refresh();
         router.replace('/');
       });
   };
